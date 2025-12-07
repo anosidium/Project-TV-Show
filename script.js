e79da53e2beb92cf6986fb609de0e7dda777fb65
Ammad <anosidium@hotmail.com>
2025-12-07 19:31:33 +0000

    Display the tv episodes in a grid

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodes = [];
+
+  for (const episode of episodeList) {
+    const card = createEpisodeCard(episode);
+    episodes.push(card);
+  }
+
+  rootElem.append(...episodes);
+}
+
+function createEpisodeCard(episode) {
+  const card = document.getElementById("episode-card").content.cloneNode(true);
+
+  card.querySelector("h3").textContent = episode.name;
+  card.querySelector("img").src = episode.image.medium;
+  const seasonNumber = String(episode.season).padStart(2, "0");
+  const episodeNumber = String(episode.number).padStart(2, "0");
+  card.querySelector("[data-season-episode-number]").textContent = `${seasonNumber}${episodeNumber}`;
+  card.querySelector("time").textContent = `${episode.runtime} minutes`;
+  card.querySelector("[data-episode-summary]").textContent = episode.summary.replace(/^<p>|<\/p>$/g, "");
+
+  return card;
 }
 
 window.onload = setup;
